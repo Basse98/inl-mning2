6b0766501912fde3df5361c633e20105c99c3408
Sebastian <sebastian.aronsson@iths.se>
2018-11-26 09:51:26 +0100

    add values from args array to constructor parameters

--- src/TestGuesser.java
+++ src/TestGuesser.java
@@ -1,7 +1,9 @@
 public class TestGuesser {
 
     public static void main(String[] args) {
-        Guesser g1 = new Guesser(0,1000);
+        int x = Integer.parseInt(args[0]);
+        int y = Integer.parseInt(args[1]);
+        Guesser g1 = new Guesser(x,y);
         g1.start();
     }
 
